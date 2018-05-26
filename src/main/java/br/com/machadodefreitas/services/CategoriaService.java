8407032dddd2e390f479c99f5e0a67dc80e427fa
Roneison Machado <roneison.machado@gmail.com>
2018-05-26 15:29:00 -0300

    Validação sintática com Bean Validation

--- src/main/java/br/com/machadodefreitas/services/CategoriaService.java
+++ src/main/java/br/com/machadodefreitas/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.machadodefreitas.domain.Categoria;
+import br.com.machadodefreitas.dto.CategoriaDTO;
 import br.com.machadodefreitas.repositories.CategoriaRepository;
 import br.com.machadodefreitas.services.exceptions.DataIntegrityException;
 import br.com.machadodefreitas.services.exceptions.ObjectNotFoundException;
@@ -52,6 +53,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO dto) {
+		return new Categoria(dto.getId(), dto.getNome());
 	}
 	
 }
