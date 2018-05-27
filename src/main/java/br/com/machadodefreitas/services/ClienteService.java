a69e635fbe3ce9566d75ed9e3c9623cb9cecde5f
Roneison Machado <roneison.machado@gmail.com>
2018-05-27 09:02:04 -0300

    PUT, DELETE e GET para Clientes

--- src/main/java/br/com/machadodefreitas/services/ClienteService.java
+++ src/main/java/br/com/machadodefreitas/services/ClienteService.java
@@ -1,12 +1,19 @@
 package br.com.machadodefreitas.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.machadodefreitas.domain.Cliente;
+import br.com.machadodefreitas.dto.ClienteDTO;
 import br.com.machadodefreitas.repositories.ClienteRepository;
+import br.com.machadodefreitas.services.exceptions.DataIntegrityException;
 import br.com.machadodefreitas.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,6 +25,40 @@ public class ClienteService {
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); 
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateDados(newObj, obj);
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um cliente porque há entidades relacionadas.");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO dto) {
+		return new Cliente(dto.getId(), dto.getNome(), dto.getEmail(), null, null);
+	}
+	
+	private void updateDados(Cliente newObj, Cliente obj) {
+		newObj.setId(obj.getId());
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 	
 }
